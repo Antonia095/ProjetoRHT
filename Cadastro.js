26e54cbd081638635b7cb088b77789e248ec768c
Antonia <antonia.josy95@gmail.com>
2019-02-05 09:20:23 -0200

    Primeiro commit

---
+++ Cadastro.js
@@ -0,0 +1,21 @@
+function Cadastro(nome, cargo, idade, dependentes, email, login, senha){
+    this.nome = nome;
+    this.cargo = cargo;
+    this.idade = idade;
+    this.dependentes = dependentes;
+    this.email = email;
+    this.login = login;
+    this.senha = senha;
+}
+
+btCadastro.onclick = function() {
+    var nome = inputNome.value;
+    var cargo = inputCargo.value;
+    var idade = inputIdade.value;
+    var dependentes = inputDependentes.value;
+    var email = inputEmail.value;
+    var login = inputLogin.value;
+    var senha = inputSenha.value;
+    
+    if()
+}
